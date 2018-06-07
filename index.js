08a73556f6cf4f2814fd6acc451e9f889ed5e05c
Gergő Móricz <mo.geryy@gmail.com>
2018-06-07 21:07:54 +0200

    Simplified & made index.js more stable, for both CLI and module use.

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 //Interactive console module
 const readline = require('readline');
+var RolandChecksum = require('roland-checksum');
 
-//Initialize command line interface
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// CLI usage
 
-//Ask for text to be shown
-rl.question('What text do you want to show? ', (answer) => {
+if (require.main === module) { // If called directly
+    //Initialize command line interface
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    //Ask for text to be shown
+    rl.question('What text do you want to show? ', (answer) => {
+        var syxBytes = api(answer); // Use module function to get .syx bytes
+
+        var syx = syxBytes.map(x => x.toString(16).toUpperCase()).join(" "); // Convert .syx bytes to .syx hex string
+
+        rl.write(syx); // Write .syx contents to command line
+        rl.close(); // Close interface
+    });
+}
+
+// Module usage
+
+/**
+ * Converts text to .syx file
+ * @param {String} text Text to convert
+ * @returns {Number[]} Result .syx hex numbers
+ */
+function api(text) {
     /*
      * Message start
      * F0 - Start of sysex message
@@ -18,32 +39,14 @@ rl.question('What text do you want to show? ', (answer) => {
      * 12 - command ID
      * 20 00 00 - command (indicating that we want to display something on the screen)
      */
-    var syx = "F0 41 10 16 12 20 00 00 ";
-    //Array of values to be used in checksum calculation
-    var hexs = ["20", "00", "00"];
-    //For each letter in input (until 20th character)
-    for (var i = 0, len = answer.length; i < len && i < 20; i++){
-        //Add hex code (ASCII) to .syx contents
-        syx += answer[i].toString().charCodeAt(0).toString(16) + " ";
-        //Push hex code to array
-        hexs.push(answer[i].toString().charCodeAt(0).toString(16))
+    var syx = [0xF0, 0x41, 0x10, 0x16, 0x12, 0x20, 0x00, 0x00];
+    // For each letter in input (until 20th character)
+    for (var i = 0, len = text.length; i < len && i < 20; i++){
+        syx.push(text[i].toString().charCodeAt(0)); // Add hex code (ASCII) to .syx contents
     }
-    var checksum = 0;
-    //For each hex value saved in the array
-    // ---- CHECKSUM CALCULATION ----
-    hexs.forEach(function(element){
-        //Add value of each hex number to checksum variable
-        checksum += parseInt(element, 16);
-    });
-    //Checksum mod 128 (same as subtracting 128 from checksum everytime it went over 127)
-    checksum = checksum % 128;
-    //Subtract remainder from 128
-    checksum = 128 - checksum;
-    // Checksum is done!
-    //Add checksum and end of sysex-message byte to .syx contents
-    syx += checksum.toString(16) + " F7"
-    //Write .syx contents to command line
-    rl.write(syx);
-    //Close interface
-    rl.close();
-});
+    syx.push(RolandChecksum(syx.slice(5))); // Checksum should not include the first 5 bytes of the command.
+    syx.push(0xF7); // Add end of sysex-message byte
+    return syx;
+}
+
+module.exports = api;
